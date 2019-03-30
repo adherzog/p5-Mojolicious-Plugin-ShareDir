552c7ba9245ed8450d5d31ec7dc88640ba9701fd
Adam Herzog <adam@herzogsc.com>
2019-03-30 13:16:31 -0400

    Initial skel from Dist::Milla.

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Mojolicious::Plugin::ShareDir - Blah blah blah
+
+# SYNOPSIS
+
+    use Mojolicious::Plugin::ShareDir;
+
+# DESCRIPTION
+
+Mojolicious::Plugin::ShareDir is
+
+# AUTHOR
+
+Adam Herzog <adam@adamherzog.com>
+
+# COPYRIGHT
+
+Copyright 2019 Adam Herzog
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Mojolicious/Plugin/ShareDir.pm
@@ -0,0 +1,41 @@
+package Mojolicious::Plugin::ShareDir;
+use strict;
+use warnings;
+
+use 5.008_005;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Mojolicious::Plugin::ShareDir - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Mojolicious::Plugin::ShareDir;
+
+=head1 DESCRIPTION
+
+Mojolicious::Plugin::ShareDir is
+
+=head1 AUTHOR
+
+Adam Herzog E<lt>adam@adamherzog.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2019 Adam Herzog
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
